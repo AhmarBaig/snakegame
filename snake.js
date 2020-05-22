496f838af4219f79ad4522dfb46c89489faf152a
Ahmar Baig <ahmarbaig2000@gmail.com>
2020-05-22 12:14:59 -0400

    Making the canvas and learning javascript animation. Getting rid of stylesheet, since it is pretty useless for this game

--- snake.js
+++ snake.js
@@ -1,1 +1,8 @@
 console.log("Snake Game");
+
+var canvas = document.getElementById("Screen");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
