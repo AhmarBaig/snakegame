8bb7e458a2fb586f99fcfb78b7f6d15fb5b6f6de
Ahmar Baig <ahmarbaig2000@gmail.com>
2020-05-22 12:41:50 -0400

    Added the snake head and the apple. No idea how to do this whole ctx thing, and its really annoying

--- snake.js
+++ snake.js
@@ -1,8 +1,23 @@
 console.log("Snake Game");
-
 var canvas = document.getElementById("Screen");
 var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+function drawSnake() {
+  ctx.beginPath();
+  ctx.rect(0, 0, 20, 20);
+  ctx.fillStyle = "green";
+  ctx.fillRect(20, 20, 20, 20);
+  ctx.stroke();
+}
+
+function drawApple() {
+  ctx.beginPath();
+  ctx.rect(40, 40, 40, 40);
+  ctx.fillStyle = "red";
+  ctx.fillRect(canvas.width/2, canvas.height/2, 20, 20);
+  ctx.stroke();
+}
+
+drawSnake();
+drawApple();
 
