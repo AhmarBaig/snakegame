2c1d2a9079f079c076fceef61a9388324b6cf72b
Ahmar Baig <ahmarbaig2000@gmail.com>
2020-05-23 02:45:59 -0400

    I dislike javascript when it comes to graphics, so I am going to use p5.js insted

--- snake.js
+++ snake.js
@@ -1,23 +1,7 @@
-console.log("Snake Game");
-var canvas = document.getElementById("Screen");
-var ctx = canvas.getContext("2d");
+function setup() {
 
-function drawSnake() {
-  ctx.beginPath();
-  ctx.rect(0, 0, 20, 20);
-  ctx.fillStyle = "green";
-  ctx.fillRect(20, 20, 20, 20);
-  ctx.stroke();
 }
 
-function drawApple() {
-  ctx.beginPath();
-  ctx.rect(40, 40, 40, 40);
-  ctx.fillStyle = "red";
-  ctx.fillRect(canvas.width/2, canvas.height/2, 20, 20);
-  ctx.stroke();
+function draw() {
+  ellipse(50, 50, 50 , 50);
 }
-
-drawSnake();
-drawApple();
-
